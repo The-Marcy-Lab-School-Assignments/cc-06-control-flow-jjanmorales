cf53e33d7fb886bec35790ae88f0331bb407fd51
Jan Morales <moralesjan40@gmail.com>
2021-10-12 14:15:33 +0000

    Completed problems 1 and 2

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 //1
 function sumOfThreeAndFive(){
-  
+    let sum = 0;
+  for(let i = 1; i < 1001; i++){
+      if(i % 3 === 0 && i % 5 === 0){
+        sum += i;
+      }
+  }
+      return sum
+}
+console.log(sumOfThreeAndFive());
+
+function greaterNum(num1, num2){
+  if(num1 > num2){
+      return num1
+  }else if(num1 === num2){
+      return "Both integers are equal"
+  }else if(!Number.isInteger(num1,num2)){
+      return null
+  }else{
+      return num2
+  }
 }
 
-function greaterNum(){
-  
-}
+console.log(greaterNum("4", "5"));
 
 function oddAndEvenInFifteen(){
-  
+  for(let i = 0; i <= 15; i++){
+      if(i % 2 === 0){
+          console.log(`${i} is even`)
+      }else{
+          console.log(`${i} is odd`)
+      }
+  }
 }
+
+oddAndEvenInFifteen();
 
 function sortThreeNums(){
   
